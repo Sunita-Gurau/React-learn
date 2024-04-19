8c650d5a04b1f7eaef537041867d497457085070
Sunita-Gurau <sunitagurau.1999@gmail.com>
2024-04-19 16:02:22 +0545

    set up prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import "./App.css";
-function App() {
+function Profile() {
   return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//       </header>
-//     </div>
-//   );
-// }
 
-// export default App;
 export default function Gallery() {
   return (
     <section>
       <h1>Amazing scientists</h1>
-      <App />
-      <App />
-      <App />
+      <Profile />
+      <Profile />
+      <Profile />
     </section>
   );
 }
